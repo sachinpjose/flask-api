d440fd9d0d6afe0ddb8f11256d18256d9f66f9a9
sachin <sachin.jose31@gmail.com>
2020-12-21 15:49:02 +0530

    added latest code

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ names = {"sachin": {"age": 25, "gender": "male"}, "test": {"age": 45, "gender": 
 
 class HelloWorld(Resource):
 
-    def get(self, name):
-        return names[name]
-        # return {"data": "The price of the product {} is  {}".format(name, price)}
+    def get(self, product, price):
+        # return names[name]
+        return {"message": "Product added successfully" , "data": "The price of the product {} is  {}".format(product, price)}
 
-api.add_resource(HelloWorld, "/hi/<string:name>")
+api.add_resource(HelloWorld, "/hi/<string:product>/<int:price>")
 
 if __name__ == '__main__':
     app.run(debug=True)
